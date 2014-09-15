1a3d2938b2dc508c5c8193b6cee986b227275874
Filmous <Filmous@Filmous-PC>
2014-09-15 12:06:46 +0700

    Spaceship can move

--- src/totheskies/ToTheSkies.java
+++ src/totheskies/ToTheSkies.java
@@ -5,12 +5,15 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 
 public class ToTheSkies extends BasicGame {
 	
 	private Image Background;
+	private Spaceship Spaceship;
+	private SpaceshipAI SpaceshipAI;
 
 	public ToTheSkies(String title) {
 		super(title);
@@ -19,18 +22,27 @@ public class ToTheSkies extends BasicGame {
 
 	@Override
 	public void render(GameContainer container, Graphics g) throws SlickException {
-		Background.draw(0,0);		
+		Background.draw(0,0);
+		Spaceship.draw();
+		SpaceshipAI.draw();
 	}
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
 		Background = new Image("res/Background.jpg");
-		
+		Spaceship = new Spaceship(360,480);
+		SpaceshipAI = new SpaceshipAI(360,50);
 	}
 
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
-		// TODO Auto-generated method stub
+		Input input = container.getInput();
+	    if (input.isKeyDown(Input.KEY_LEFT)) {
+	      Spaceship.moveLeft();
+	    }
+	    if (input.isKeyDown(Input.KEY_RIGHT)) {
+	      Spaceship.moveRight();
+	    }
 		
 	}
 	
